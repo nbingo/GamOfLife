842e6e891d4a9fdaa83219f11d7e812fc399706c
nbingo <nringach1@hwemail.com>
2016-01-27 14:30:34 -0800

    Can create battle soups.

---
+++ Users/Nomi/git/GameOfLife/Game of Life/src/BattleSoupBuilder.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+/**
+ * Creates the PRIMORDIAL SOUP
+ * @since Jan 21, 2016
+ * @author Nomi Ringach
+ * @version 1.0.0
+ */
+public class BattleSoupBuilder 
+{
+	public static void main(String[] args) throws IOException
+	{
+		Scanner k = new Scanner(System.in);
+		System.out.println("Please input \"ROWS COLUMNS FILENAME PROBABILITY-OF-LIFE\"");
+		int rows = k.nextInt(), cols = k.nextInt();
+		String filename = k.next();
+		double probability = k.nextDouble();
+		PrintWriter write = new PrintWriter(new BufferedWriter(new FileWriter(filename + ".psp")), true);
+		write.println(rows + " " + cols);
+		for (int r = 0; r < rows; r++)
+		{
+			for (int c = 0; c < cols; c++)
+				if((int)(Math.random()+probability)>=1)
+					if ((int)(Math.random()) >= 0.5)
+						write.print(2);
+					else
+						write.print(1);
+			write.println();
+		}
+		k.close();
+		write.close();
+	}
+}
